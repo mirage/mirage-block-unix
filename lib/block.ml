2a1af24038cd5f0edf99287f8bc81054e8a5f792
David Scott <dave@recoil.org>
2016-04-04 15:41:29 +0100

    Use Lwt_cstruct.complete from cstruct.1.3.0
    Signed-off-by: David Scott <dave@recoil.org>

--- lib/block.ml
+++ lib/block.ml
@@ -154,25 +154,8 @@ let disconnect t = match t.fd with
 
 let get_info { info } = return info
 
-let complete op fd buffer =
-  let open Lwt in
-  let ofs = buffer.Cstruct.off in
-  let len = buffer.Cstruct.len in
-  let buf = buffer.Cstruct.buffer in
-  let rec loop acc fd buf ofs len =
-    op fd buf ofs len >>= fun n ->
-    let len' = len - n in
-    let acc' = acc + n in
-    if len' = 0 || n = 0
-    then return acc'
-    else loop acc' fd buf (ofs + n) len' in
-  loop 0 fd buf ofs len >>= fun n ->
-  if n = 0 && len <> 0
-  then fail End_of_file
-  else return ()
-
-let really_read = complete Lwt_bytes.read
-let really_write = complete Lwt_bytes.write
+let really_read fd = Lwt_cstruct.complete (Lwt_cstruct.read fd)
+let really_write fd = Lwt_cstruct.complete (Lwt_cstruct.write fd)
 
 let lwt_wrap_exn t op offset ?buffer f =
   let fatalf fmt = Printf.ksprintf (fun s ->
