5333a9259c15b7b7d2dca1ef206eb1076152bf1e
David Scott <dave@recoil.org>
2017-02-12 10:40:49 +0000

    benchmark: add the argument '-split'
    This simulates scatter-gather I/O which is supported by the BLOCK
    interface but not by the current implementation here.
    
    The arguments are:
    
      -bs x: means pass `x` bytes to `write` in one call
      -split y: means supply the bytes as a list of Cstruct.t values, with
        a maximim size of `y` bytes
    
    For example `-bs 65536 -split 512` means supply 64KiB in each call
    as a list of 512 byte buffers.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- lib_test/benchmark.ml
+++ lib_test/benchmark.ml
@@ -16,12 +16,20 @@
 
 (* A simple file copy benchmark *)
 
-let copy ~src ~dst ~bs ~buffered ~threads =
+let split_into buf into =
+  let rec loop acc remaining =
+    if Cstruct.len remaining = 0 then List.rev acc else begin
+      let to_take = min into (Cstruct.len remaining) in
+      loop (Cstruct.sub remaining 0 to_take :: acc) (Cstruct.shift remaining to_take)
+    end in
+  loop [] buf
+
+let copy ~src ~dst ~bs ~split ~buffered ~threads =
   let open Lwt.Infix in
   Block.connect ~buffered src
   >>= fun src ->
   (* Create the destination if it doesn't exist *)
-  Lwt_unix.openfile dst [ Lwt_unix.O_CREAT; Lwt_unix.O_WRONLY ] 0o0644
+  Lwt_unix.openfile dst [ Lwt_unix.O_CREAT; Lwt_unix.O_TRUNC; Lwt_unix.O_WRONLY ] 0o0644
   >>= fun fd ->
   Lwt_unix.close fd
   >>= fun () ->
@@ -58,13 +66,16 @@ let copy ~src ~dst ~bs ~buffered ~threads =
         let start_sector = !sector in
         sector := Int64.add !sector this_time;
         let buf = Cstruct.sub pages 0 (Int64.to_int (this_time) * info.Mirage_block.sector_size) in
-        Block.read src start_sector [ buf ]
+        let bufs = match split with
+          | None -> [ buf ]
+          | Some into -> split_into buf into in
+        Block.read src start_sector bufs
         >>= function
         | Error _ -> failwith (Printf.sprintf "Block.read %Ld failed" start_sector)
         | Ok () ->
         resize_to_atleast (Int64.add start_sector this_time)
         >>= fun () ->
-        Block.write dst start_sector [ buf ]
+        Block.write dst start_sector bufs
         >>= function
         | Error _ -> failwith (Printf.sprintf "Block.write %Ld failed" start_sector)
         | Ok () ->
@@ -86,16 +97,19 @@ let _ =
   let src = ref "benchmark.src" in
   let dst = ref "benchmark.dst" in
   let bs = ref 512 in
+  let split = ref None in
   let buffered = ref true in
   let threads = ref 1 in
   Arg.parse [
     "-src", Arg.Set_string src, Printf.sprintf "source filename (default %s)" !src;
     "-dst", Arg.Set_string dst, Printf.sprintf "destination filename (default %s)" !dst;
     "-bs", Arg.Set_int bs, Printf.sprintf "block size (default %d)" !bs;
+    "-split", Arg.Int (fun x -> split := Some x), "split each block into a list of fragments";
     "-buffered", Arg.Set buffered, Printf.sprintf "buffered (default %b)" !buffered;
     "-threads", Arg.Set_int threads, Printf.sprintf "threads (default %d)" !threads;
   ] (fun x ->
       Printf.fprintf stderr "Unrecognised argument: %s\nSee -help for usage.\n" x;
       exit 1;
     ) "A simple file copy benchmark";
-  Lwt_main.run @@ copy ~src:(!src) ~dst:(!dst) ~bs:(!bs) ~buffered:(!buffered) ~threads:(!threads)
+  Logs.set_reporter (Logs_fmt.reporter ());
+  Lwt_main.run @@ copy ~src:(!src) ~dst:(!dst) ~bs:(!bs) ~split:(!split) ~buffered:(!buffered) ~threads:(!threads)
